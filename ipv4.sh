1b48cb249159809073b7cf4de280f78a1d48b288
Rodrigo F Martins <martins.drg@gmail.com>
2014-02-13 22:59:02 -0200

    Nome do arquivo de base de testes é criado dinamicamente e é validado

--- ipv4.sh
+++ ipv4.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-#ARQ_TEST="$0-test.txt"
-ARQ_TEST="ipv4-test.txt"
+ARQ_TEST=$(echo $0 | sed -r 's/(\.\/|\.sh$)//g')
+ARQ_TEST="${ARQ_TEST}-test.txt"
+
+if test ! -f $ARQ_TEST
+then
+   echo "Arquivo base de testes não encontrado."
+   exit
+fi
 
 OCTETO="((1[0-9]|[1-9])?[0-9]|2([0-4][0-9]|5[0-5]))"
+IPV4="${OCTETO}(\.${OCTETO}){3}"
 
-IPV4="$OCTETO(\.$OCTETO){3}"
-
-grep --color=always -E \"$IPV4\" $ARQ_TEST
+grep --color=always -E \"${IPV4}\" ${ARQ_TEST}
 echo -e "\n"
-grep --color=always -Ev \"$IPV4\" $ARQ_TEST
+grep --color=always -Ev \"${IPV4}\" ${ARQ_TEST}
